ef2a1b78569c09ff5d078b264e84f2ac1440cb7c
JohanChan <johanleo97@gmail.com>
2021-03-25 00:21:00 -0600

    ReporteGenerado

--- src/macros.asm
+++ src/macros.asm
@@ -76,6 +76,7 @@ obtenerNumero macro arreglo
     UnidadNegativo:
         mov al,arreglo[1]
 		SUB al,30h
+        neg al
 		jmp Salir
     Negativo:
         mov al,arreglo[1]
@@ -108,12 +109,15 @@ LOCAL LeerNumero, endTexto
 	endTexto:
 		mov bx,si
 endm
+
 IntToString macro numero, arreglo
-LOCAL Unidades,Decenas, Salir
+LOCAL Unidades,Decenas, Salir, Negativos
 	xor ax,ax
 	xor bx,bx
 
 	mov al,numero
+    cmp al, 99
+    ja  Negativos
 	cmp al, 9 
 	ja Decenas
 
@@ -128,16 +132,250 @@ LOCAL Unidades,Decenas, Salir
 		add al,30h ;le suma 30h a al, al cociente 
 		add ah,30h ; le suma 30h al residuo 
 		mov arreglo[0],al ;decenas
-		mov arreglo[1],ah ;unidades 
+		mov arreglo[1],ah ;unidades
 
 		jmp Salir
+    Negativos:
+        neg al
+        mov arreglo[0], 2Dh
+        jmp NegDecenas
+    NegDecenas:
+		mov bl,10
+		div bl     ;divide entre 10 las decenas
+		add al,30h ;le suma 30h a al, al cociente 
+		add ah,30h ; le suma 30h al residuo 
+		mov arreglo[1],al ;decenas
+		mov arreglo[2],ah ;unidades 
+		jmp Salir    
 
 	Salir:
 endm
-devolverDecimal macro u, d, numero
-        mov al, d
-        mov bl, 10
-        mul bl
-        add al, u
-        mov numero, al
+concatenarValor macro arreglo1, arreglo2, separador
+    LOCAL verificarEspacio, guardarCaracteres, guardarUnCaract, guardarDosCaract,guardarTresCaract, fin, insertarInicio
+    xor si, si
+    verificarEspacio:
+        tamaString arreglo1
+        cmp bl, 0
+            je insertarInicio
+        mov al, arreglo1[si]
+        cmp al, 24h
+            je guardarCaracteres
+        inc si
+        jmp verificarEspacio
+
+    insertarInicio: 
+        push si ; me guarda el valor de 'si' de esta macro
+        tamaString arreglo2
+        pop si  ; recupero el valor de 'si' de esta macro 
+        cmp bl, 2
+            je guardarDosCaract
+        cmp bl, 1   
+            je guardarUnCaract
+        cmp bl, 3
+            je guardarTresCaract
+
+    guardarCaracteres:
+        push si ; me guarda el valor de 'si' de esta macro
+        tamaString arreglo2
+        pop si  ; recupero el valor de 'si' de esta macro 
+        mov al, separador
+        mov arreglo1[si], al
+        inc si
+        cmp bl, 2
+            je guardarDosCaract
+        cmp bl, 3
+            je guardarTresCaract
+        
+    guardarUnCaract:
+        ;xor si, si
+        mov al, arreglo2[0]
+        mov arreglo1[si], al
+        jmp fin
+
+    guardarDosCaract:
+        ;xor si, si
+        mov al, arreglo2[0]
+        mov arreglo1[si], al
+        inc si
+        mov al, arreglo2[1]
+        mov arreglo1[si], al
+        jmp fin
+
+    guardarTresCaract:
+        mov al, arreglo2[0]
+        mov arreglo1[si], al
+        inc si
+        mov al, arreglo2[1]
+        mov arreglo1[si], al
+        inc si
+        mov al, arreglo2[2]
+        mov arreglo1[si], al
+        jmp fin        
+    fin: 
+endm
+guardarOperacion macro arreglo, caracter
+    LOCAL inicio, fin, guardarCaracter, guardardosDatos, guardartresDatos, guardarDatos
+    xor di,di 
+    xor si,si
+    inicio:
+        mov al, arreglo[di]
+        cmp al, 24h
+            je guardarDatos
+        inc di
+        jmp inicio
+    guardarDatos:
+        tamaString caracter
+        cmp bl, 1
+           je guardarCaracter
+        cmp bl, 2
+            je guardardosDatos
+        cmp bl, 3
+            je guardartresDatos
+
+    guardarCaracter:
+        mov al, caracter[0]
+        mov arreglo[di], al
+        jmp fin
+    guardardosDatos:
+        mov al, caracter[0]
+        mov arreglo[di], al
+        inc di
+        mov al, caracter[1]
+        mov arreglo[di], al
+        jmp fin
+    guardartresDatos:
+        mov al, caracter[0]
+        mov arreglo[di], al
+        inc di
+        mov al, caracter[1]
+        mov arreglo[di], al
+        inc di
+        mov al, caracter[2]
+        mov arreglo[di], al
+        jmp fin
+
+    fin:
+endm
+
+
+guardarOperaciones macro arreglo1, arreglo2
+    LOCAL inicio, fin, verificarEspacio, guardar
+    xor di, di
+    xor si, si
+    verificarEspacio:
+        mov al, arreglo1[di]
+        cmp al, 24h
+            je guardar
+        inc di
+        jmp verificarEspacio
+    guardar:
+        mov al, arreglo2[si]
+        cmp al, 24h
+            je fin
+        mov arreglo1[di], al
+        inc di
+        inc si
+        jmp guardar
+    fin:
+endm
+
+crearArchivo macro ruta, handler
+    xor ax,ax
+    mov ah, 3ch
+    mov cx, 00h
+    lea dx, ruta
+    int 21h
+    mov handler, ax
+endm
+
+escribirArchivo macro handler, data, numeroBytes
+    mov ah, 40h
+    mov bx, handler
+    mov cx, numeroBytes
+    lea dx, data
+    int 21h
+endm
+
+cerrarArchivo macro handler
+    mov ah, 3Eh
+    mov bx, handler
+    int 21h
+endm
+
+escribirFecha macro handler, fecha, separador
+    ; Obtengo la fecha del sistema
+    MOV AH,2AH    ; To get System Date
+    INT 21H
+    MOV AL,DL     ; Day is in DL
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov fecha[0],dl
+    mov fecha[1],al
+
+    mov al, separador
+    mov fecha[2], al
+
+    MOV AH,2AH    ; To get System Date
+    INT 21H
+    MOV AL,DH     ; Month is in DH
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov fecha[3],dl
+    mov fecha[4],al
+
+    mov al, separador
+    mov fecha[5], al
+
+
+    MOV AH,2AH    ; To get System Date
+    INT 21H
+    ADD CX,0F830H ; To negate the effects of 16bit value,
+    MOV AX,CX     ; since AAM is applicable only for AL (YYYY -> YY)
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov fecha[6],dl
+    mov fecha[7],al
+
+    escribirArchivo handler, fecha ,SIZEOF fecha 
+
+endm
+escribirHora macro handler, hora, separadorHora
+    ; Obtengo la hora del sistema
+    MOV AH,2CH    ; To get System Time
+    INT 21H
+    MOV AL,CH     ; Hour is in CH
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov hora[0],dl
+    mov hora[1],al
+
+    mov al, separadorHora
+    mov hora[2], al
+
+    MOV AH,2CH    ; To get System Time
+    INT 21H
+    MOV AL,CL     ; Minutes is in CL
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov hora[3],dl
+    mov hora[4],al
+
+    mov al, separadorHora
+    mov hora[5], al
+
+    MOV AH,2CH    ; To get System Time
+    INT 21H
+    MOV AL,DH     ; Seconds is in DH
+    AAM
+    MOV BX,AX
+    CALL DISP
+    mov hora[6],dl
+    mov hora[7],al
+
+    escribirArchivo handler, hora ,SIZEOF hora
 endm
--- src/practica.asm
+++ src/practica.asm
@@ -2,6 +2,7 @@ include macros.asm
 .model small
 .stack
 .data 
+    encabezadoPrincipal db 10,"UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"ESCUELA DE CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1 A",10,13,"SECCION B",10,13,"PRIMER SEMESTRE 2021",10,13,"Johan Leonel Chan Toledo",10,13,"201603052",10,13,"Primera Practica Assembler",10,"$"
     encabezado BYTE "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",10,"$"
     encabezado2 BYTE "%%%%%%%%% MENU PRINCIPAL %%%%%%%%",10,"$"
     opcionCarga BYTE "%% 1. Cargar Archivo           %%",10,"$"
@@ -12,7 +13,7 @@ include macros.asm
     opcionelegida BYTE "Elija una opcion: ",10,"$" 
     prueba BYTE 10,"Opcion calculadora ",10,"$"
     menuFactorial BYTE 10,"Ingrese numero entre (00-04): ",10,"$"
-    despedida BYTE 10,"Adios Vaquero!! ",10,"$"
+    despedida BYTE 10,"         -------- Adios Vaquero!! --------          ",10,"$"
     saltoLinea db " ",10,"$"
     numopcion db 0
     signo db ?
@@ -31,14 +32,49 @@ include macros.asm
     signoIgual db "= ","$"
     singoFact db "! ","$"
     numeroCompleto db 3 dup("$")
+    deseaGuardar db 10,"Desea Guardar (S/N): ",10,"$"
+    resultadosGuardados db 100 dup("$")
+    
+    mensajeGuardado db "Valor guardado ",10,"$"
+    separador db ",","$"
+    operacionesGuardadas db 200 dup("$")
+    pivoteOperaciones db 200 dup("$")
+    mensajeReport db "       -------- Reporte creado!! --------       ",10,"$"
+    errorCrear db "       -------- Error al crear archivo!! --------       ",10,"$"
+    errorEscribir db "       -------- Error al crear archivo!! --------       ",10,"$"
+    rutaReporte db "reporte.html"
+    handlerReporte dw ?
+    etiquetaHTML db "<html>"
+    encabezadoHTML db "<h1>Practica 3 Arqui 1 Seccion B</h1>","<p>Estudiante: Johan Leonel Chan Toledo</p>","<p>Carnet: 201603052</p>"
+    tabla db "<table border=1><thead><tr><th>Id Operacion</th><th>Operacion</th><th>Resultado</th></tr></thead>"
+    columna db "<td>"
+    columnaC db "</td>"
+    fila db "<tr>"
+    filaC db "</tr>"
+    tablaC db "</table>"
+    etiquetaHTMLC db "</html>"
+    fecha db 15 dup(" ")
+    hora db 9 dup(" ")
+    separadorFecha db "/"
+    separadorHora db ":"
+    labelFecha db "Fecha: "
+    labelHora db "Hora: "
+    P db "<p>"
+    pC db "</p>"
+    indice db 0
+    Operacion db "Operacion "
+    char db "$"
+
 .code 
 
 main proc
+
     menu:
     mov   ax, seg @data     
     mov   ds, ax  
 
     ; muestra el menu completo
+    imprimir encabezadoPrincipal
     imprimir encabezado
     imprimir encabezado2
     imprimir opcionCarga
@@ -59,6 +95,8 @@ main proc
     cmp numopcion, 33h
     je factorial
 
+    cmp numopcion, 34h
+    je crearReporte
     ; otro comentario alv
     cmp numopcion, 35h
     je Salir
@@ -75,7 +113,7 @@ main proc
         imprimir pedirNum
         limpiar numeroCompleto, SIZEOF numeroCompleto, 24h
         obtenerCadena numeroCompleto
-        imprimir numeroCompleto
+        guardarOperacion pivoteOperaciones, numeroCompleto
         obtenerNumero numeroCompleto
         mov resultao, al
         limpiar numeroCompleto, SIZEOF numeroCompleto,24h 
@@ -83,17 +121,23 @@ main proc
         imprimir pedirSigno
         getDato
         mov signo, al ; signo = al
+        guardarOperacion pivoteOperaciones, signo
         imprimir saltoLinea
         imprimir pedirNum
         obtenerCadena numeroCompleto
+        guardarOperacion pivoteOperaciones, numeroCompleto
         obtenerNumero numeroCompleto
+
         mov numero, al
-        imprimir numeroCompleto
         limpiar numeroCompleto, SIZEOF numeroCompleto,24h 
         call funcionCalculadora
     factorial:
         call factorial
     Salir:
+        mov cx, 4
+        saltos:
+            imprimir saltoLinea    
+        loop saltos
         imprimir despedida
         mov ah, 4ch
         xor al, al
@@ -132,7 +176,6 @@ factorial proc
             imprimir signoPor
             jmp imprimirDigito
         resultado:
-            
             imprimir saltoLinea
             imprimir printResultado
             mov al, numeroFact
@@ -167,20 +210,17 @@ funcionCalculadora proc
 
         getDato        
         mov signo, al
+        guardarOperacion pivoteOperaciones, signo
         cmp signo, 3Bh
         je resultado
 
         imprimir saltoLinea
         imprimir pedirNum
-
-        getDato
-        sub al, 30h
-        mov d, al 
-        getDato
-        sub al, 30h
-        mov u, al
-        devolverDecimal u, d, numero
-
+        obtenerCadena numeroCompleto
+        guardarOperacion pivoteOperaciones, numeroCompleto
+        obtenerNumero numeroCompleto
+        mov numero, al
+        limpiar numeroCompleto, SIZEOF numeroCompleto,24h 
         jmp irSigno
     suma:
         mov al, resultao ; al = resultao
@@ -211,6 +251,112 @@ funcionCalculadora proc
         IntToString resultao, numeroCompleto
         imprimir numeroCompleto
         imprimir saltoLinea
-        call main
+        jmp guardar
+    guardar:
+        imprimir saltoLinea
+        imprimir deseaGuardar
+        getDato
+        cmp al, 53h 
+            je saveData
+        cmp al, 73h
+            je saveData
+        limpiar pivoteOperaciones, SIZEOF pivoteOperaciones, 24h
+        jmp main
+    saveData:
+        concatenarValor resultadosGuardados, numeroCompleto, separador
+        guardarOperaciones operacionesGuardadas, pivoteOperaciones
+        limpiar pivoteOperaciones, SIZEOF pivoteOperaciones, 24h
+        imprimir saltoLinea
+        imprimir resultadosGuardados
+        imprimir saltoLinea
+        imprimir operacionesGuardadas
+        imprimir saltoLinea
+        imprimir mensajeGuardado
+        jmp main
 funcionCalculadora endp
+
+crearReporte proc 
+    mov cx, 4
+    xor si, si
+    xor di, di
+    saltos:
+        imprimir saltoLinea
+        loop saltos
+    encabezadoReporte:
+        crearArchivo rutaReporte, handlerReporte
+        escribirArchivo handlerReporte, etiquetaHTML, SIZEOF etiquetaHTML
+        escribirArchivo handlerReporte, encabezadoHTML, SIZEOF encabezadoHTML
+
+        escribirArchivo handlerReporte, p, SIZEOF p
+        escribirArchivo handlerReporte, labelFecha, SIZEOF labelFecha
+        escribirFecha handlerReporte, fecha, separadorFecha
+        escribirArchivo handlerReporte, pC, SIZEOF pC
+
+        escribirArchivo handlerReporte, p, SIZEOF p
+        escribirArchivo handlerReporte, labelHora, SIZEOF labelHora
+        escribirHora handlerReporte, hora, separadorHora
+        escribirArchivo handlerReporte, pC, SIZEOF pC
+        escribirArchivo handlerReporte, tabla, SIZEOF tabla
+
+        escribirArchivo handlerReporte, fila, SIZEOF fila
+        escribirArchivo handlerReporte, columna, SIZEOF columna
+        escribirArchivo handlerReporte, Operacion, SIZEOF Operacion
+        escribirArchivo handlerReporte, columnaC, SIZEOF columnaC
+        escribirArchivo handlerReporte, columna, SIZEOF columna
+        inc indice
+        jmp crearFila
+    nuevaFila:       
+        escribirArchivo handlerReporte, columnaC, SIZEOF columnaC
+        escribirArchivo handlerReporte, filaC, SIZEOF filaC
+        escribirArchivo handlerReporte, fila, SIZEOF fila
+        escribirArchivo handlerReporte, columna, SIZEOF columna
+        escribirArchivo handlerReporte, Operacion, SIZEOF Operacion
+        escribirArchivo handlerReporte, columnaC, SIZEOF columnaC
+        escribirArchivo handlerReporte, columna, SIZEOF columna
+        inc indice
+        jmp crearFila
+    nuevaColumna:
+        escribirArchivo handlerReporte, columnaC, SIZEOF columnaC
+        escribirArchivo handlerReporte, columna, SIZEOF columna
+        jmp crearColumna
+    crearColumna:   
+        mov al, resultadosGuardados[si]
+        mov char, al
+        inc si
+        cmp al, ','
+        je nuevaFila
+        cmp al, '$'
+        je pieReporte
+        escribirArchivo handlerReporte, char, SIZEOF char
+        jmp crearColumna
+    crearFila:
+        mov al, operacionesGuardadas[di]
+        mov char, al
+        inc di
+        cmp al, ';'
+        je nuevaColumna
+        cmp al, '$'
+        je pieReporte
+        escribirArchivo handlerReporte, char, SIZEOF char
+        jmp crearFila
+    pieReporte:
+        escribirArchivo handlerReporte, columnaC, SIZEOF columnaC
+        escribirArchivo handlerReporte, filaC, SIZEOF filaC
+        escribirArchivo handlerReporte, tablaC, SIZEOF tablaC
+        escribirArchivo handlerReporte, etiquetaHTMLC, SIZEOF etiquetaHTMLC
+        cerrarArchivo handlerReporte
+        imprimir mensajeReport
+    fin:
+        jmp main
+crearReporte endp
+
+DISP PROC
+    MOV DL,BH      ; Since the values are in BX, BH Part
+    ADD DL,30H     ; ASCII Adjustment
+
+    MOV al,BL      ; BL Part 
+    ADD al,30H     ; ASCII Adjustment
+    RET
+DISP ENDP      ; End Disp Procedure
+
 end main
